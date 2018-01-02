7226417ce4fbb02a653494f79cd87a4d609f12fa
xablen <xavier.parfait@gmail.com>
2018-01-02 15:59:17 +0100

    mvc

--- app/src/main/java/com/example/xavier/topquiz/controller/GameActivity.java
+++ app/src/main/java/com/example/xavier/topquiz/controller/GameActivity.java
@@ -1,13 +1,15 @@
-package com.example.xavier.topquiz;
+package com.example.xavier.topquiz.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.xavier.topquiz.R;
+
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game);
     }
 }
